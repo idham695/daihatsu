d3550bb01352b11725ada1b0d89ff0a0a3d29102
idham kholid <idhamkholid965@gmail.com>
2020-02-22 15:09:06 +0700

    add event

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
+var element = document.getElementById("container");
 function openNav() {
+    element.classList.add("utility");
     document.getElementById("sidenav").style.width = "250px";
 }
 
 function closeNav() {
+    element.classList.remove("utility");
     document.getElementById("sidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
