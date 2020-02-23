7a2608b37fbd18e1e05bb4e2650c922addc824f9
idham kholid <idhamkholid965@gmail.com>
2020-02-23 17:32:36 +0700

    add css media

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 var element = document.getElementById("container");
-function openNav() {
-    element.classList.add("utility");
-    document.getElementById("sidenav").style.width = "300px";
-}
+
 
 function closeNav() {
     element.classList.remove("utility");
+    document.getElementById("sidenav").style.transition = "0.3s";
     document.getElementById("sidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
+
+
+// icon 
 feather.replace();
