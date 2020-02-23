fbb6e5320dc7884d5db5e84ef460aae50f427044
idham kholid <idhamkholid965@gmail.com>
2020-02-23 17:52:57 +0700

    css media query

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 var element = document.getElementById("container");
 
+function openNav(){
+    element.classList.add("utility");
+    document.getElementById("sidenav").style.transition = "0.3s";
+    document.getElementById("sidenav").style.width = "250px";
+}
 
 function closeNav() {
     element.classList.remove("utility");
